435b709bddea43153a011a3e0b10b269b47a17a5
William Robert Simpson <will.simpson85@gmail.com>
2018-11-19 19:32:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,9 @@ function updatePlaylist(obj,key,value){
 obj[key] = value
  return obj
 }
-function removeFromPlaylist(obj,key,value){
-  delete obj.key;
-  return obj
+function removeFromPlaylist(playlist,artistName){
+
+  delete playlist.artistName;
+
+  return playlist
 }
